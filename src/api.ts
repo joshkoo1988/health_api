258930fb54b25c3d45acbebfeac8453e51f6d49d
joshkoo1988 <joshkoo1988@gmail.com>
2025-07-25 23:34:41 -0400

    created fetch pt data from web api url

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import axiosRetry from "axios-retry";
 
 import { HEALTH_API_KEY, WEB_API_URL } from "./constants";
-import { PaginationInfo, Patient } from "./types";
+import { Patient, PaginatedResponse } from "./types";
 
 // Setup axios with retries
 axiosRetry(axios, {
@@ -16,3 +16,34 @@ axiosRetry(axios, {
     );
   }
 });
+
+
+// Exported function that the app calls
+export async function fetchAllPatients(): Promise<Patient[]> {
+  const allPatients: Patient[] = [];
+  let page = 1;
+  let hasNext = true;
+
+  while (hasNext) {
+    try {
+      const response = await axios.get<PaginatedResponse>(
+        `${WEB_API_URL}/patients?page=${page}&limit=5`,
+        {
+          headers: {
+            'x-api-key': HEALTH_API_KEY,
+          },
+        }
+      );
+
+      const { data, pagination } = response.data;
+      allPatients.push(...data);
+      hasNext = pagination.hasNext;
+      page++;
+    } catch (error) {
+      console.error(`Failed to fetch page ${page}:`, (error as any).message);
+      throw error;
+    }
+  }
+
+  return allPatients;
+}
